cc6cec6bf49c2b84b49ed9eaea0e9480c8deed6c
Mayck Gomes <mayck.gomes2901@gmail.com>
2025-05-09 15:29:46 -0300

    começo projeto

---
+++ app/src/main/java/mayckgomes/com/racetimeapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package mayckgomes.com.racetimeapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Red = Color(0xFFD70000)
+
+val DarkGray = Color(0xFF313131)
+val DarkBackground = Color(0xFF0E0E0E)
+
+val LightGray = Color(0xFFB3B3B3)
+val LightBackgound = Color(0xFFF0F0F0)
